aaf251bdc4e9b715e158c71d6b38c55e93b3e074
Henderson Abreu <henderson.abreu.silva@gmail.com>
2022-03-16 15:19:57 -0300

    Scrool

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,3 +42,20 @@ if(target.length) {
     animeScroll();
   }, 200));
 }
+
+function upscreen() {
+  window.scrollTo({
+      top:0,
+      behavior: 'smooth'
+  });
+}
+function scrollbutton() {
+  if(window.scrollY === 0) {
+      //ocultar botão
+      document.querySelector('.up').style.display = 'none';
+  } else {
+      //mostrar botão
+      document.querySelector('.up').style.display = 'block';
+  }
+}
+window.addEventListener('scroll',scrollbutton);
